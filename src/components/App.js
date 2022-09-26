2336a91d82211a8d3c2090f9a05970981e6ec708
Aman Raj <101209058+henryaman016@users.noreply.github.com>
2022-09-26 18:51:09 +0530

    Revert "Update package-lock.json and App.js"

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,13 @@
-import React, { Component, useState } from "react";
+import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  const showFn = () => {
-    setShow(true);
-  };
-
   return (
     <div id="main">
-      <button id="click" onClick={showFn}>
-        Click
-      </button>
-
-      {show && (
-        <p id="para">
-          {" "}
-          Hello, I've learnt to use the full-stack evaluation tool. This makes
-          me so happy
-        </p>
-      )}
+      // Do not alter the main div
     </div>
   );
 }
+
 
 export default App;
